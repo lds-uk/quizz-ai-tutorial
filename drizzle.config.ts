11a288bace43d12d639c401d3326b0dd0d92e0be
Lazaro Dos Santos <165022597+lds-uk@users.noreply.github.com>
2024-10-12 14:39:52 +0100

    should be working

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,6 +7,6 @@ export default {
   dbCredentials: {
     connectionString:
       process.env.DATABASE_URL ||
-      "postgres://postgres:postgres@localhost:5432/postgres",
+      "postgresql://postgres.nsygrjkgqesdysvxrogh:Catbbclondon2024!@aws-0-eu-west-2.pooler.supabase.com:6543/postgres",
   },
 } satisfies Config;
